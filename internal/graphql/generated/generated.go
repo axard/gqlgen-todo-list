aa00ace488d0f9d06db3959f1f7e83592a89fec1
Andrey Tarasov <andrey13.tarasov@yandex.ru>
2020-10-30 18:47:38 +0300

    Поменял cхему данных
    Изменён тип поля UserID для ввода NewTodo: String -> ID

--- internal/graphql/generated/generated.go
+++ internal/graphql/generated/generated.go
@@ -224,7 +224,7 @@ type Query {
 
 input NewTodo {
   text: String!
-  userId: String!
+  userId: ID!
 }
 
 type Mutation {
@@ -1774,7 +1774,7 @@ func (ec *executionContext) unmarshalInputNewTodo(ctx context.Context, obj inter
 			var err error
 
 			ctx := graphql.WithPathContext(ctx, graphql.NewPathWithField("userId"))
-			it.UserID, err = ec.unmarshalNString2string(ctx, v)
+			it.UserID, err = ec.unmarshalNID2string(ctx, v)
 			if err != nil {
 				return it, err
 			}
